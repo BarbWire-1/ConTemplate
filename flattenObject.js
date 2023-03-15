9417103abfd5d76b6a7268e222b0dca884fdbd83
Barbara Kälin <67426447+BarbWire-1@users.noreply.github.com>
2023-03-15 16:45:17 +0100

    .

--- flattenObject.js
+++ flattenObject.js
@@ -4,16 +4,16 @@
  */
 //#Source https://bit.ly/2neWfJ2 
 const flattenObject = (obj, prefix = '') =>
-    Object.keys(obj).reduce((acc, k) => {
-        const pre = prefix.length ? prefix + '.' : '';
-        if (typeof obj[ k ] === 'object') Object.assign(acc, flattenObject(obj[ k ], pre + k));
-        else acc[ pre + k ] = obj[ k ];
+    Object.keys(obj).reduce((acc, key) => {
+        const parentKey = prefix.length ? prefix + '.' : '';
+        if (typeof obj[ key ] === 'object') Object.assign(acc, flattenObject(obj[ key ], parentKey + key));
+        else acc[ parentKey + key ] = obj[ key ];
         return acc;
     }, {});
-console.log(flattenObject({ a: { b: { c: 1 } }, d: 1 }));
+console.log(flattenObject({ a: { b: { c: 1 } }, d: 2 }));
 /**
  {
  "a.b.c": 1,
- "d": 1
+ "d": 2
  }
  */
