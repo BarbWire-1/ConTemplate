107c0f48f5b5bfae282264b02bf3e347430cd82a
Barbara Kälin <67426447+BarbWire-1@users.noreply.github.com>
2023-03-20 11:56:15 +0100

    GOT REVERSE WORKING

--- filterData.js
+++ filterData.js
@@ -2,64 +2,175 @@
  *   Copyright (c) 2023 
  *   All rights reserved.
  */
+// Define a class called DataObserver1
 class DataObserver1 {
-    constructor (dataSource, proto, excludeProperties = []) {
-        this.data = dataSource.map(obj => this.filterObj(obj, excludeProperties));
+
+    // Define a constructor function that takes three arguments: dataSource, proto, and exclude
+    constructor (dataSource, proto, exclude = []) {
+
+        // Map over the dataSource array to create a new array of filtered objects
+        this.data = dataSource.map((obj) =>
+
+            // Call the filterObj method defined below on each object in dataSource
+            // filterObj takes three arguments: the current object, an array of properties to exclude, and a prototype object
+            this.filterObj(obj, exclude, Object.getPrototypeOf({}))
+        );
+
+        // Set the 'proto' and 'observers' properties on the new DataObserver1 instance
         this.proto = proto;
         this.observers = [];
     }
 
-    filterObj(obj, excludeProperties) {
+    // Define a helper method called filterObj that takes three arguments: obj, excludeProperties, and prototype
+    filterObj(obj, excludeProperties, prototype) {
+
+        // Define an empty object called 'filteredObj'
         const filteredObj = {};
+
+        // Loop over each property in obj
         for (const key in obj) {
 
+            // Get the value of the current property
             const value = obj[ key ];
 
-            if (Array.isArray(value)) {
-                //console.log(value)
-                filteredObj[ key ] = value.map((item) =>
-                    this.filterObj(item, excludeProperties)
-                );
-            }
+            // Check if the current property should be excluded based on the excludeProperties array
             if (!excludeProperties.includes(key)) {
-                //const value = obj[ key ];
-                // console.log([ key ])
-                // console.log(Array.isArray(value))// always false
 
-                //} else if (typeof value === "object" && value !== null) {
+                // If the value of the current property is an object (and not null), recursively call filterObj on it
                 if (typeof value === "object" && value !== null) {
                     const nestedFilteredObj = this.filterObj(
                         value,
                         excludeProperties.map((prop) =>
                             prop.startsWith(key + ".") ? prop.slice(key.length + 1) : prop
-                        )
+                        ),
+                        prototype
                     );
                     Object.assign(filteredObj, { [ key ]: nestedFilteredObj });
+
+                    // If the value of the current property is an array, loop over each item in the array
+                } else if (Array.isArray(value)) {
+                    const arr = [];
+                    for (let i = 0; i < value.length; i++) {
+                        const item = value[ i ];
+
+                        // If the item is an object (and not a string), recursively call filterObj on it
+                        if (typeof item !== "string") {
+                            const filteredItem = this.filterObj(
+                                item,
+                                excludeProperties,
+                                prototype
+                            );
+                            arr.push(filteredItem);
+
+                            // If the item is a string, add it to the new array without modification
+                        } else {
+                            arr.push(item);
+                        }
+                    }
+                    Object.assign(filteredObj, { [ key ]: arr });
+
+                    // If the value of the current property is neither an object nor an array, add it to filteredObj as is
                 } else {
                     filteredObj[ key ] = value;
                 }
             }
-
         }
-        return filteredObj;
+
+        // Create a new object with 'filteredObj' as its prototype and add a '__
+
+        // Create a new object with 'filteredObj' as its prototype
+        // with a reference to the initial Object
+        const objWithProto = Object.create(prototype, {
+            ...Object.getOwnPropertyDescriptors(filteredObj),
+            __originalObj: { value: obj },
+        });
+        return objWithProto;
     }
-
-
-
-
 }
 
-// Example usage
-const dataSource = [
+
+// Usage
+const data = [
     { name: 'John', address: { street: '123 Main St', city: 'Anywhere' }, hobbies: [ { name: 'Fishing', category: 'Outdoor' } ], age: 30 },
     { name: 'Jane', address: '456 Elm St', hobbies: [ { name: 'Reading', category: 'Indoor' } ], age: 25 },
     { name: 'Bob', address: '789 Oak St', hobbies: [ { name: 'Running', category: 'Outdoor' }, 'another Hobby' ], age: 40 },
 ];
-// exclude works for nested objects, but not for arrays
-const exclude = [ 'address.street', 'hobbies.0' ];
-const observer = new DataObserver1(dataSource, {}, exclude);
-//console.log(observer.data);
 
-dataSource[ 0 ].name = 'Happy'
+const doNotNeed = [ 'address', 'hobbies.0' ];
+const observer = new DataObserver1(
+    dataSource = data,
+    proto = {},
+    exclude = doNotNeed);
 
-//console.log(observer.data);
+console.log(observer.data[ 0 ].name === data[ 0 ].name);// true
+data[ 0 ].name = 'TWEET'
+console.log(observer.data[ 0 ].name)// as no getters yet => John
+console.log(observer.data[ 0 ].name === data[ 0 ].name);// false
+
+console.log(observer.data)
+
+
+class DataObserver2 {
+    constructor (dataSource, proto) {
+        this.proto = proto;
+        this.observers = [];
+        this.data = dataSource.map(obj => this.createProxy(obj));
+        console.log(this.data)
+    }
+
+    createProxy(obj) {
+        const protoKeys = Object.keys(this.proto);
+        const filteredObj = {};
+        protoKeys.forEach(key => {
+            if (key in obj) {
+                Object.defineProperty(filteredObj, key, {
+                    get() {
+                        return obj[ key ];
+                    },
+                    set(value) {
+                        obj[ key ] = value;
+                    },
+                    enumerable: true
+                });
+            } else {
+                Object.defineProperty(filteredObj, key, {
+                    get() {
+                        return this.proto[ key ];
+                    },
+                    enumerable: true
+                });
+            }
+        });
+        return filteredObj;
+    }
+
+}
+
+const testProto = {
+    name: 'name',
+    address: {},
+    //age: 0,
+    sealed: true,
+    hobbies: []
+};
+
+const data2 = [
+    { name: 'John', address: { street: '123 Main St', city: 'Anywhere' }, hobbies: [ { name: 'Fishing', category: 'Outdoor' } ], age: 30 },
+    { name: 'Jane', address: '456 Elm St', hobbies: [ { name: 'Reading', category: 'Indoor' } ], age: 25 },
+    { name: 'Bob', address: '789 Oak St', hobbies: [ { name: 'Running', category: 'Outdoor' }, 'another Hobby' ], age: 40 },
+];
+
+const observer2 = new DataObserver2(testData, testProto);
+
+console.log(observer2.data[ 0 ]); // { name: 'John', address: { street: '123 Main St', city: 'Anywhere' }, hobbies: [ { name: 'Fishing', category: 'Outdoor' } ], age: 30 }
+console.log(observer2.data[ 0 ].name); // 'John'
+
+// Modify the original data
+// data2[ 0 ].name = 'Jim';
+// data2[ 0 ].address.city = 'Somewhere';
+// 
+// testData[0].name = 'Test Name'
+// console.log(observer2.data[ 0 ].name); // 'Jim'
+// console.log(observer2.data[ 0 ].address); // 'Somewhere'
+// console.log(JSON.stringify(observer2.data[ 0 ].hobbies[ 0 ] = 'testing'))
+// console.log(JSON.stringify(observer2.data[ 0 ].hobbies = 'testing'))
