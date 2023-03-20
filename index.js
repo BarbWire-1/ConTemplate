b9b3fbc0c1e01136aa1b31f56dc26b46197f1bdb
Barbara Kälin <67426447+BarbWire-1@users.noreply.github.com>
2023-03-20 11:19:37 +0100

    GOT PROTO WORKING

--- index.js
+++ index.js
@@ -695,3 +695,69 @@ console.log(observer.data[ 0 ].name)// as no getters yet => John
 console.log(observer.data[ 0 ].name === data[ 0 ].name);// false
 
 console.log(observer.data)
+
+
+class DataObserver2 {
+    constructor (dataSource, proto) {
+        this.proto = proto;
+        this.observers = [];
+        this.data = dataSource.map(obj => this.createProxy(obj));
+        console.log(this.data)
+    }
+
+    createProxy(obj) {
+        const protoKeys = Object.keys(this.proto);
+        const filteredObj = {};
+        protoKeys.forEach(key => {
+            if (key in obj) {
+                Object.defineProperty(filteredObj, key, {
+                    get() {
+                        return obj[ key ];
+                    },
+                    set(value) {
+                        obj[ key ] = value;
+                    },
+                    enumerable: true
+                });
+            } else {
+                Object.defineProperty(filteredObj, key, {
+                    get() {
+                        return this.proto[ key ];
+                    },
+                    enumerable: true
+                });
+            }
+        });
+        return filteredObj;
+    }
+
+}
+
+const testProto = {
+    name: 'name',
+    address: {},
+    //age: 0,
+    sealed: true,
+    hobbies: []
+};
+
+const data2 = [
+    { name: 'John', address: { street: '123 Main St', city: 'Anywhere' }, hobbies: [ { name: 'Fishing', category: 'Outdoor' } ], age: 30 },
+    { name: 'Jane', address: '456 Elm St', hobbies: [ { name: 'Reading', category: 'Indoor' } ], age: 25 },
+    { name: 'Bob', address: '789 Oak St', hobbies: [ { name: 'Running', category: 'Outdoor' }, 'another Hobby' ], age: 40 },
+];
+
+const observer2 = new DataObserver2(testData, testProto);
+
+console.log(observer2.data[ 0 ]); // { name: 'John', address: { street: '123 Main St', city: 'Anywhere' }, hobbies: [ { name: 'Fishing', category: 'Outdoor' } ], age: 30 }
+console.log(observer2.data[ 0 ].name); // 'John'
+
+// Modify the original data
+data2[ 0 ].name = 'Jim';
+data2[ 0 ].address.city = 'Somewhere';
+
+testData[0].name = 'Test Name'
+console.log(observer2.data[ 0 ].name); // 'Jim'
+console.log(observer2.data[ 0 ].address); // 'Somewhere'
+console.log(JSON.stringify(observer2.data[ 0 ].hobbies[ 0 ] = 'testing'))
+console.log(JSON.stringify(observer2.data[ 0 ].hobbies = 'testing'))
