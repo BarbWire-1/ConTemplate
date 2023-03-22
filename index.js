12a108c95d8fe811aa525458ef582d01bd48092d
Barbara Kälin <67426447+BarbWire-1@users.noreply.github.com>
2023-03-22 18:54:32 +0100

    ???

--- index.js
+++ index.js
@@ -186,6 +186,7 @@ class DataObserver {
                             break;
 
                         default:
+                            
                             break;
                     }
 
